9991923757fe07f9701f6a1195bce2c72e03d7b3
salmanahmedxyz <salmanahmedx@hotmail.com>
2022-02-14 16:02:17 +0600

    add remove button functionality

--- shopping-cart-main/app.js
+++ shopping-cart-main/app.js
@@ -1,18 +1,61 @@
+
+const subtotal = document.querySelector(".subtotal")
+const tax = document.querySelector(".tax")
+const total = document.querySelector(".total")
+
+
+function updateShoppingCart(product, price, isIncreasing) {
+    const plusBtn = document.querySelector(`.${product}-plus-btn`)
+    const minusBtn = document.querySelector(`.${product}-minus-btn`)
+    const itemCount = document.querySelector(`.${product}-count`)
+    const productPrice = document.querySelector(`.${product}-price`)
+    let itemCountValue = itemCount.value;
+
+    if (isIncreasing) {
+        itemCountValue = parseInt(itemCountValue) + 1;
+        itemCount.value = itemCountValue;
+    } else {
+        if (itemCountValue < 1) {
+            itemCount.value = 0;
+        } else {
+            itemCountValue = parseInt(itemCountValue) - 1;
+            itemCount.value = itemCountValue;
+        }
+    }
+
+    let newProductPrice = price * itemCountValue;
+    productPrice.textContent = `$${newProductPrice}`;
+
+    //invoice
+    let newSubtotal = newProductPrice;
+    let newTaxAmount = newProductPrice / 10;
+    let newTotal = newSubtotal + newTaxAmount;
+
+    subtotal.textContent = `$${newProductPrice}`;
+    tax.textContent = `$${newTaxAmount}`;
+    total.textContent = `$${newTotal}`;
+
+}
+
 const plusBtns = document.querySelectorAll(".plus-btn")
 const minusBtns = document.querySelectorAll(".minus-btn")
-const itemCount = document.querySelector("#item-count")
-let itemCountValue = itemCount.value;
 
 plusBtns.forEach(plusBtn => plusBtn.addEventListener("click", (e) => {
-    itemCountValue = parseInt(itemCountValue) + 1; //variable containing the value from a attribute
-    itemCount.value = itemCountValue; //selecting and changing attribute
-}))
-minusBtns.forEach(minusBtn => minusBtn.addEventListener("click", (e) => {
-    if (itemCountValue < 1) {
-        itemCount.value = 0;
-    } else {
-        itemCountValue = parseInt(itemCountValue) - 1;
-        itemCount.value = itemCountValue;
+    const parentElement = e.target.parentElement;
+    if (parentElement.classList.contains("phone-plus-btn")) {
+        updateShoppingCart("phone", 1219, true)
+    }
+    if (parentElement.classList.contains("case-plus-btn")) {
+        updateShoppingCart("case", 59, true)
     }
 }))
 
+minusBtns.forEach(minusBtn => minusBtn.addEventListener("click", (e) => {
+    const parentElement = e.target.parentElement;
+    if (parentElement.classList.contains("phone-minus-btn")) {
+        updateShoppingCart("phone", 1219, false)
+    }
+    if (parentElement.classList.contains("case-minus-btn")) {
+        updateShoppingCart("case", 59, false)
+    }
+}))
